d0e84e6dda2a5a294edc5ba5334a0981d02119b8
Kendall George <kendallanngeorge@gmail.com>
2021-02-25 17:49:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function logShout(hello) {
 function logWhisper(HELLO) {
   console.log(HELLO.toLowerCase());
 }
+
+function sayHiToGrandma() {
+  var uppercase = "HELLO!"
+  var lowercase = 'hello!'
+  var mixedCase = "I love you, Grandma."
+  console.log();
+  if (uppercase.toUpperCase() === uppercase) {
+      return "I can't hear you!";
+  } else if (lowercase.toLowerCase() === lowercase) {
+      return "YES INDEED!";
+  } else if (mixedCase.toLowerCase() === mixedCase && mixedCase.toUpperCase() === mixedCase) {
+      return "I love you, too."
+  }
+}
